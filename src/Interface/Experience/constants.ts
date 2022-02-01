28e0cad192947b8c56948761c93c59e5ba96f9a8
Rahul Halder <hrahul2605@gmail.com>
2022-02-01 13:33:09 +0530

    upd: experience
    Signed-off-by: Rahul Halder <hrahul2605@gmail.com>

--- src/Interface/Experience/constants.ts
+++ src/Interface/Experience/constants.ts
@@ -3,14 +3,14 @@ import { WorkProps } from "./types";
 export const Works: WorkProps[] = [
   {
     start: "September, 2021",
-    end: "Present",
+    end: "January, 2021",
     post: "Software Development Intern",
     org: "Trell",
     description: [
       "Working with e-commerce team, developing modules and services using php, magento, graphql",
     ],
     url: ["https://shop.trell.co/"],
-    icon: 'https://cdn.trell.co/images/orig/hwiuhdmr53tyjNkskQjFzXKk0vPePa3T.svg'
+    icon: "https://cdn.trell.co/images/orig/hwiuhdmr53tyjNkskQjFzXKk0vPePa3T.svg",
   },
   {
     start: "May, 2021",
@@ -21,7 +21,7 @@ export const Works: WorkProps[] = [
       "The project gives user the ability to create voice channels that would allow users to quickly enter and leave pre-existing voice calls from Rocket.Chat's own UI, enabling a more practical usage of audio features for team collaboration.",
     ],
     url: ["https://summerofcode.withgoogle.com/projects/#5318205117038592"],
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/GSoC-icon.svg/1200px-GSoC-icon.svg.png'
+    icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/GSoC-icon.svg/1200px-GSoC-icon.svg.png",
   },
   {
     start: "Sep 5, 2020",
@@ -33,6 +33,6 @@ export const Works: WorkProps[] = [
       "Other Technologies used Redux, Redux Thunk, Eslint, React Router, dayjs, Antd Library",
     ],
     url: ["https://www.skillbee.com"],
-    icon: 'https://jobs.skillbee.com/icon.png'
+    icon: "https://jobs.skillbee.com/icon.png",
   },
 ];
